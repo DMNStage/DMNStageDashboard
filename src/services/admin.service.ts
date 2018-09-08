519422b04c2b45d7704802e4b595538898ef7c24
AbdellahASKI <abdellahaski@gmail.com>
2018-09-08 15:47:40 +0100

    -Auth Service

--- src/services/admin.service.ts
+++ src/services/admin.service.ts
@@ -1,34 +1,33 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Admin} from '../model/admin.model';
+import {AuthService} from './auth.service';
 
 @Injectable()
 export class AdminService {
 
     currentAdmin: any;
-    private readonly access_token = '36cb41bb-5274-4538-b739-804e53f1e5f9';
 
-    constructor(public http: HttpClient) {
-
+    constructor(public http: HttpClient, public authservice: AuthService) {
     }
 
     getAdmins() {
-        return this.http.get('http://localhost:8088/users?type=admin&access_token=' + this.access_token);
+        return this.http.get('http://localhost:8088/users?type=admin&access_token=' + this.authservice.accessToken);
     }
 
     getAdmin(id: number) {
-        return this.http.get('http://localhost:8088/users/' + id + '?access_token=' + this.access_token);
+        return this.http.get('http://localhost:8088/users/' + id + '?access_token=' + this.authservice.accessToken);
     }
 
     saveAdmin(admin: Admin) {
-        return this.http.post('http://localhost:8088/users/admin?access_token=' + this.access_token, admin);
+        return this.http.post('http://localhost:8088/users/admin?access_token=' + this.authservice.accessToken, admin);
     }
 
     editAdmin(admin: Admin) {
-        return this.http.put('http://localhost:8088/users/admin/' + admin.id + '?access_token=' + this.access_token, admin);
+        return this.http.put('http://localhost:8088/users/admin/' + admin.id + '?access_token=' + this.authservice.accessToken, admin);
     }
 
     deleteAdmin(id: number) {
-        return this.http.delete('http://localhost:8088/users/' + id + '?access_token=' + this.access_token);
+        return this.http.delete('http://localhost:8088/users/' + id + '?access_token=' + this.authservice.accessToken);
     }
 }
